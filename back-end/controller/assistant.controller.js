5fde81383d2063832016ce837e8925b76ad36bdf
epetecof <epetecof@br.ibm.com>
2018-11-01 17:09:45 -0300

    using pipeline env variables

--- back-end/controller/assistant.controller.js
+++ back-end/controller/assistant.controller.js
@@ -3,15 +3,23 @@ const request = require('request')
 
 const AssistantV1 = require('watson-developer-cloud/assistant/v1');
 
-console.log("Using following assistant username: "+process.env.assistant_username);
-console.log("Using following assistant password: "+process.env.assistant_password);
-console.log("Using following assistant url: "+process.env.assistant_url);
+console.log(JSON.stringify(process.env))
+const assistant_username = process.env.WATSON_ASSISTANT_USERNAME
+console.log("Using following assistant username: "+assistant_username);
+const assistant_password = process.env.WATSON_ASSISTANT_PASSWORD
+console.log("Using following assistant password: "+assistant_password);
+const assistant_url = process.env.WATSON_ASSISTANT_URL || "https://gateway.watsonplatform.net/assistant/api/"
+console.log("Using following assistant url: "+assistant_url);
+const assistant_version = process.env.WATSON_ASSISTANT_VERSION || '2018-10-25'
+console.log("Using following assistant version: "+assistant_version);
+const assistant_workspace = process.env.WATSON_ASSISTANT_WORKSPACE
+console.log("Using following assistant workspace: "+assistant_workspace);
 //Assistant Credentials
 const assistant = new AssistantV1({
-    username: process.env.assistant_username,
-    password: process.env.assistant_password,
-    url: process.env.assistant_url,
-    version: '2018-10-25'
+    username: assistant_username,
+    password: assistant_password,
+    url: assistant_url,
+    version: assistant_version
 });
 
 //Send the input to conversation and return the response
